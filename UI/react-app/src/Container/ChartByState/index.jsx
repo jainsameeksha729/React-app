fbb6b36386d95ad5f4c35a9c2ee8feeb8a1111f2
sameeksha <sameeksha@stream.space>
2020-12-01 13:53:56 +0530

    resolved chart issue

--- UI/react-app/src/Container/ChartByState/index.jsx
+++ UI/react-app/src/Container/ChartByState/index.jsx
@@ -7,34 +7,19 @@ import { getAllCollege, getCollegeByState } from "../../actions/college.action";
 import "./index.scss";
 // import Card from "./components/card";
 import Pagination from "react-paginate";
-import {withRouter, Link, NavLink} from 'react-router-dom'
-const perPage = 5
+import "chartjs-plugin-labels";
+
+import { withRouter, Link, NavLink } from "react-router-dom";
+const perPage = 5;
 const options = {
   responsive: true,
-  // plugins: {
-  //   legend: true,
-  //   title: true,
-  // },
-  legend: {
-    position: 'top',
-},
-title: {
-    display: true,
-    // text: 'By States'
-},
-  animation: {
-    duration: 0,
-    animateRotate: true,
-    animateScale: false,
-  },
-  cutoutPercentage: 80,
-  rotation: 0,
-  circumference: 360,
-  elements: {
-    arc: {
-      backgroundColor: "rgb(255, 0, 0)",
-      borderColor: "rgb(255, 255, 255)",
-      borderWidth: 2,
+  maintainAspectRatio: false,
+  plugins: {
+    labels: {
+      render: "percentage",
+      fontColor: ["black"],
+      precision: 2,
+      arc: true,
     },
   },
 };
@@ -42,14 +27,14 @@ const ChartByState = (props) => {
   const [sharable, setSharable] = useState(null);
   const [collegeByStates, setCollegeByStates] = useState([]);
   const [canvasClicked, setCanvasClicked] = useState(false);
-  const [reload, setReload] = useState(true)
-  const [currentPage, setCurrentPage] = useState(1)
-  const [state, setState] = useState("")
-  const [pageCount, setPageCount] = useState(0)
+  const [reload, setReload] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [state, setState] = useState("");
+  const [pageCount, setPageCount] = useState(0);
   // const [doughnut, setDoughnutChart] = useState(null)
   useEffect(() => {
-    if(reload) {
-      setReload(false)
+    if (reload) {
+      setReload(false);
       updateChart();
     }
   });
@@ -114,21 +99,7 @@ const ChartByState = (props) => {
       }
       let backgroundColors = backColor();
 
-      console.log("backgroundColors", backgroundColors);
-      //   backgroundColors = [
-      //     'rgba(255, 99, 132, 0.2)',
-      //     'rgba(54, 162, 235, 0.2)',
-      //     'rgba(255, 206, 86, 0.2)',
-      //     // 'rgba(75, 192, 192, 0.2)',
-      //     // 'rgba(153, 102, 255, 0.2)',
-      //     // 'rgba(255, 159, 64, 0.2)'
-      // ]
-
-      // backgroundColors = ['rgba(128,31,166,0.2)', 'rgba(154,69,69,0.2)', 'rgba(65,164,254,0.2)']
-
-      console.log("backgroundColors", backgroundColors);
-
-      //   let data = Object.values(states)
+      
       console.log(
         "STATESSS",
         allColleges,
@@ -140,12 +111,10 @@ const ChartByState = (props) => {
         labels: labels,
         datasets: [
           {
-            // label: labels,
             data: percentges,
             backgroundColor: backgroundColors,
-            borderColor: ["rgb(255, 255, 255)"],
-            // order:Number,
-            // borderWidth: 2,
+            borderWidth: 2,
+            
           },
         ],
       };
@@ -153,13 +122,20 @@ const ChartByState = (props) => {
       // And for a doughnut chart
       var myDoughnutChart = new Chart(ctx, {
         type: "doughnut",
-        data: data,
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              data: percentges,
+              backgroundColor: backgroundColors,
+              borderWidth: 3,
+            },
+          ],
+        },
         options: options,
       });
 
-      // setDoughnutChart(myDoughnutChart)
-      canvas.onclick = (e) => handleCanvasClicked(e, myDoughnutChart)
-      
+      canvas.onclick = (e) => handleCanvasClicked(e, myDoughnutChart);
     } catch (error) {
       console.log("ERROR", error);
     }
@@ -167,29 +143,34 @@ const ChartByState = (props) => {
     // return data
   };
 
-  function handleCanvasClicked (evt, doughnut) {
+  function handleCanvasClicked(evt, doughnut) {
     console.log("evttt", evt);
     var activePoints = doughnut.getElementsAtEvent(evt);
     console.log("activePoints", activePoints);
 
     if (activePoints[0]) {
+      var clickedDatasetIndex = activePoints[0]._datasetIndex;
+      var clickedElementIndex = activePoints[0]._index;
+      var value =
+        doughnut.data.datasets[clickedDatasetIndex].data[clickedElementIndex];
+
+      console.log("ssssssssssssssss", value);
       var chartData = activePoints[0]["_chart"].config.data;
       var idx = activePoints[0]["_index"];
 
       var label = chartData.labels[idx];
-      var value = chartData.datasets[0].data[idx];
-      var color = chartData.datasets[0].backgroundColor[idx]; //Or any other data you wish to take from the clicked slice
+      // var value = chartData.datasets[0].data[idx];
+      // var color = chartData.datasets[0].backgroundColor[idx]; //Or any other data you wish to take from the clicked slice
 
       getCollegeByState(label, currentPage).then((data) => {
         setCanvasClicked(true);
         setCollegeByStates(data.result);
-        setState(label)
-        setPageCount(parseInt(data.total) / perPage)
+        setState(label);
+        setPageCount(parseInt(data.total) / perPage);
         console.log("statessss", data.result);
       });
-      
     }
-  };
+  }
 
   const handlePageClick = (event) => {
     getCollegeByState(state, currentPage).then((data) => {
@@ -198,20 +179,23 @@ const ChartByState = (props) => {
 
       console.log("statessss", data.result);
     });
-    setCurrentPage(event.selected)
-
+    setCurrentPage(event.selected);
   };
 
   return (
-      <section className="state">
-        <div className="container-fluid">
-          <div className="row justify-content">
-            <div className="col-12">
-              <canvas id="myChart" style={{position: 'relative', height:'40vh', width:'80vw'}}></canvas>
-            </div>
+    <section className="state">
+      <div className="container-fluid">
+        <div className="row justify-content">
+          <div className="col-12">
+            <canvas
+              id="myChart"
+              style={{ position: "relative", height: "40vh", width: "80vw" }}
+            ></canvas>
           </div>
+        </div>
 
-          {canvasClicked && <div className="row justify-content">
+        {canvasClicked && (
+          <div className="row justify-content">
             <div className="col-12 fileshare-table">
               <table className="table table-striped table-bordered">
                 <thead>
@@ -233,7 +217,6 @@ const ChartByState = (props) => {
                       No Of Students
                       <span className="icon-upside-down ss-text-medium-2"></span>
                     </th>
-                    
                   </tr>
                 </thead>
                 <tbody>
@@ -252,14 +235,15 @@ const ChartByState = (props) => {
                     )
                     .map((collegeByState, i) => {
                       return (
-                        <tr key={"fl" + i} 
-                        onClick={() => {
-                          console.log("FURRRRRRRR", collegeByState._id)
-                          return (                          
-                            props.history.push(`/app/state/${collegeByState._id}`)
-                          );
-                        }
-                        }>
+                        <tr
+                          key={"fl" + i}
+                          onClick={() => {
+                            console.log("FURRRRRRRR", collegeByState._id);
+                            return props.history.push(
+                              `/app/state/${collegeByState._id}`
+                            );
+                          }}
+                        >
                           <td
                             className="ss-text-medium ss-text-dark ss-text-weight-300"
                             id={`fn${i}`}
@@ -280,31 +264,31 @@ const ChartByState = (props) => {
                           <td className="ss-text-medium ss-text-dark ss-text-weight-300">
                             {collegeByState.noOfStudents}
                           </td>
-                          
                         </tr>
                       );
                     })}
                 </tbody>
               </table>
               <div className="ss-pagination-container">
-                  <Pagination
-                    previousLabel={<span className="icon-left-arrow"></span>}
-                    nextLabel={<span className="icon-right-arrow"></span>}
-                    breakLabel={"..."}
-                    pageCount={pageCount}
-                    marginPagesDisplayed={2}
-                    pageRangeDisplayed={5}
-                    onPageChange={(e) => handlePageClick(e)}
-                    containerClassName={"ss-pagination"}
-                    pageClassName={"ss-pagination-item ss-text-medium"}
-                    // activeClassName={this.state.currentPageClass}
-                    forcePage={parseInt(currentPage)}
-                  ></Pagination>
-                </div>
+                <Pagination
+                  previousLabel={<span className="icon-left-arrow"></span>}
+                  nextLabel={<span className="icon-right-arrow"></span>}
+                  breakLabel={"..."}
+                  pageCount={pageCount}
+                  marginPagesDisplayed={2}
+                  pageRangeDisplayed={5}
+                  onPageChange={(e) => handlePageClick(e)}
+                  containerClassName={"ss-pagination"}
+                  pageClassName={"ss-pagination-item ss-text-medium"}
+                  // activeClassName={this.state.currentPageClass}
+                  forcePage={parseInt(currentPage)}
+                ></Pagination>
+              </div>
             </div>
-          </div>}
-        </div>
-      </section>
+          </div>
+        )}
+      </div>
+    </section>
   );
 };
 
